7ed235907933d49641bf4804d3cf94a93a541208
Pedrovin1 <pedro.zancan18@gmail.com>
2025-05-01 15:59:48 -0300

    reestruturacao classe QuadroAnotacao

--- server/src/PushIt/Models/QuadroAnotacao.cs
+++ server/src/PushIt/Models/QuadroAnotacao.cs
@@ -1,10 +1,22 @@
 public class QuadroAnotacao
 {
-    public string TextoInterno { get; }
+    public string id { get; }
+    public int x { get; }
+    public int y { get; }
+    public int width { get; }
+    public int height { get; }
+    public string text { get; }
+    public string colour { get; }
 
-
-    public QuadroAnotacao(string textoInterno)
+    public QuadroAnotacao(string id, int x, int y, int width, int height, string text, string colour)
     {
-        this.TextoInterno = textoInterno;
+         this.id = id;
+         this.x = x;
+         this.y = y;
+         this.width = width;
+         this.height = height;
+         this.text = text;
+         this.colour = colour;
     }
+    
 }
